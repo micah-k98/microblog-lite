4980a909f807cb3afd9c5bb6c745f4b7200cd6cb
micah-k98 <146677653+micah-k98@users.noreply.github.com>
2024-01-03 11:51:09 -0700

    Added sort functionality

--- my-posts-page/my-posts.js
+++ my-posts-page/my-posts.js
@@ -2,6 +2,7 @@
 
 let postService, likesService, authService;
 let postTemplate, postsContainer, postSection;
+let allPosts;
 
 document.addEventListener("DOMContentLoaded", () => {
     // Set variables
@@ -12,8 +13,10 @@ document.addEventListener("DOMContentLoaded", () => {
     postTemplate = document.getElementById("postTemplate");
     postsContainer = document.getElementById("postsContainer");
 
+    const sortSelect = document.getElementById("sortSelect");
+
     // Register events
-    
+    sortSelect.addEventListener("change", getAllPosts);
 
     // Call these functions when the page loaded
     getAllPosts();
@@ -21,12 +24,10 @@ document.addEventListener("DOMContentLoaded", () => {
 
 async function getAllPosts() {
     const userName = sessionStorage.username;
-    const allPosts = await postService.getByUser(userName);
+    allPosts = await postService.getByUser(userName);
 
-    // Will sort date from the most current to oldest post
-    allPosts.sort((left, right) => {
-        return new Date(right.createdAt) - new Date(left.createdAt)
-    })
+    // Sorting
+    sortPosts();
 
     postsContainer.innerText = "";
 
@@ -142,6 +143,25 @@ function isItLiked(post, likePostButton) {
         }
     }
     return likeId;
+}
+
+// For sorting
+function sortPosts() {
+    switch (sortSelect.value) {
+        case "0":
+        case "recent":
+            allPosts.sort((left, right) => {
+                return new Date(right.createdAt) - new Date(left.createdAt)
+            })
+            break;
+        case "popularity":
+            allPosts.sort((left, right) => {
+                return new Date(right.likes.length) - new Date(left.likes.length)
+            })
+            break;
+        default:
+            break;
+    }
 }
 
 // For logout
