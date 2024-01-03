7541a7fd52de1baa5be0832fc2129088ee3daf91
micah-k98 <146677653+micah-k98@users.noreply.github.com>
2024-01-03 15:53:58 -0700

    Updated JavaScript and page logo

--- profile-page/profile.js
+++ profile-page/profile.js
@@ -40,11 +40,11 @@ async function displayUserInfo() {
     userData = await usersService.getCurrent(userName);
     
     userFullName.innerText = userData.fullName;
-    if (userData.bio == null) {
+    if (userData.bio == null || userData.bio == "") {
         userBio.innerText = "Spill the tea on your awesome self!"
         userBio.insertAdjacentHTML("afterend", `<svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" fill="currentColor" class="bi bi-arrow-down" viewBox="0 0 16 16">
-        <path fill-rule="evenodd" d="M8 1a.5.5 0 0 1 .5.5v11.793l3.146-3.147a.5.5 0 0 1 .708.708l-4 4a.5.5 0 0 1-.708 0l-4-4a.5.5 0 0 1 .708-.708L7.5 13.293V1.5A.5.5 0 0 1 8 1"/>
-      </svg>`)
+            <path fill-rule="evenodd" d="M8 1a.5.5 0 0 1 .5.5v11.793l3.146-3.147a.5.5 0 0 1 .708.708l-4 4a.5.5 0 0 1-.708 0l-4-4a.5.5 0 0 1 .708-.708L7.5 13.293V1.5A.5.5 0 0 1 8 1"/>
+            </svg>`)
     }
     else userBio.innerText = userData.bio;
 }
@@ -57,7 +57,7 @@ async function saveNewPost() {
     const posted = await postService.add(postInfo);
 
     // Direct it to my-posts page
-    location.href = `/my-posts-page/my-posts.html?username=${userData.username}`
+    location.href = `/my-posts-page/my-posts.html`;
 }
 
 // For logout
