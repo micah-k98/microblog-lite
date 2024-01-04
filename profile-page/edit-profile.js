681bc64b41f561c0a570a11beaf71de5aa561dcf
micah-k98 <146677653+micah-k98@users.noreply.github.com>
2024-01-04 03:15:11 -0700

    Added current password verification

--- profile-page/edit-profile.js
+++ profile-page/edit-profile.js
@@ -2,7 +2,7 @@
 
 let authService, usersService;
 let loginData, userData;
-let newUsername, newName, newBio, newPassword, updateButton, myModal;
+let newUsername, newName, newBio, currentPassword, newPassword, updateButton, myModal;
 
 document.addEventListener("DOMContentLoaded", ()=> {
     // Set variables
@@ -20,10 +20,11 @@ document.addEventListener("DOMContentLoaded", ()=> {
     newUsername = document.getElementById("newUsername");
     newName = document.getElementById("newName");
     newBio = document.getElementById("newBio");
+    currentPassword = document.getElementById("currentPassword");
     newPassword = document.getElementById("newPassword");
 
     updateButton = document.getElementById("updateButton");
-    myModal = bootstrap.Modal.getOrCreateInstance('#updatedMessage');
+    // myModal = bootstrap.Modal.getOrCreateInstance('#updatedMessage');
 
     // Register events
     updateButton.addEventListener("click", updateButtonClicked)
@@ -48,6 +49,7 @@ async function updateButtonClicked(event) {
         username: newUsername.value,
         fullName: newName.value,
         bio: newBio.value,
+        currentPassword: currentPassword.value,
         password: newPassword.value
     }
     
@@ -59,7 +61,23 @@ async function updateButtonClicked(event) {
             "password": newData.password
         }
         await usersService.changeLoginData(userLogin);
+
+        document.getElementById("forModal").innerHTML = `<div class="modal fade" id="updatedMessage" data-bs-backdrop="static" data-bs-keyboard="false" tabindex="-1" aria-hidden="true">
+                                                            <div class="modal-dialog">
+                                                                <div class="modal-content">
+                                                                    <div class="modal-header">
+                                                                        <h1 class="modal-title fs-5">User info has been successfully updated!</h1>
+                                                                        <button type="button" class="btn-close" data-bs-dismiss="modal" aria-label="Close" onclick="closeMessage()"></button>
+                                                                    </div>
+                                                                </div>
+                                                            </div>
+                                                        </div>`
+        myModal = bootstrap.Modal.getOrCreateInstance('#updatedMessage');
         myModal.show();
+    }
+    else {
+        console.log(updated);
+        alert("Please enter the correct password")
     }
 }
 
